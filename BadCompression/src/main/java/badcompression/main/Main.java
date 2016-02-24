350420c519a164052f90d3e1287a4a88bdf3b74c
Antti Haapaniemi <antti.haapaniemi@helsinki.fi>
2016-02-24 23:52:50 +0200

    Viikko5 - korjaus: mainista testaus pois.

--- BadCompression/src/main/java/badcompression/main/Main.java
+++ BadCompression/src/main/java/badcompression/main/Main.java
@@ -19,27 +19,27 @@ public class Main {
     
     public static void main(String[] args) throws Exception {     
     
-        String fileForCompression = "/home/antti/random3.dat";
-        String compressTo = "/home/antti/random3.dat.badComp";
-        String uncompressTo = "/home/antti/random3_2.dat";
-
-        Compressor comp = new HuffmanCompressionByte(); //or new HuffmanCompressionUTF8();
-        CompressionResults results = comp.compress(new URI("file://" + fileForCompression), new URI("file://" + compressTo));
-        if (results.isFailure()) {
-            System.out.println("FILURE:");
-            System.out.println(results.getFailureReason());
-        } else {
-            System.out.println("Compression successful.");
-        }
-
-        
-        results = comp.uncompress(new URI("file://" + compressTo), new URI("file://" + uncompressTo));
-        if (results.isFailure()) {
-            System.out.println("FAILURE:");
-            System.out.println(results.getFailureReason());
-        } else {
-            System.out.println("Uncompress successful.");
-        }
+//        String fileForCompression = "";
+//        String compressTo = "";
+//        String uncompressTo = "";
+//
+//        Compressor comp = new HuffmanCompressionByte(); //or new HuffmanCompressionUTF8();
+//        CompressionResults results = comp.compress(new URI("file://" + fileForCompression), new URI("file://" + compressTo));
+//        if (results.isFailure()) {
+//            System.out.println("FILURE:");
+//            System.out.println(results.getFailureReason());
+//        } else {
+//            System.out.println("Compression successful.");
+//        }
+//
+//        
+//        results = comp.uncompress(new URI("file://" + compressTo), new URI("file://" + uncompressTo));
+//        if (results.isFailure()) {
+//            System.out.println("FAILURE:");
+//            System.out.println(results.getFailureReason());
+//        } else {
+//            System.out.println("Uncompress successful.");
+//        }
     }
     
     
